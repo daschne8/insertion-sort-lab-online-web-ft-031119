fdf94a6f0d9292cb28eae62eaec4e9b327de76cd
Daniel Schneider <daschne8@gmail.com>
2019-08-24 16:39:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
-
+  let minIndex = 0
+  let minNum = array[0]
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < minNum) {
+      minNum = array[i]
+      minIndex = i
+    }
+  }
+  return minNum
 }
 
 function insertionSort(array){
